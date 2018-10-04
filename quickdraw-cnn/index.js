3f1e932343000d66308e12c9b9d8e07368b9d598
Albert Zheng <lisong.zheng@gmail.com>
2018-10-05 00:08:45 +0800

    Commit

--- quickdraw-cnn/index.js
+++ quickdraw-cnn/index.js
@@ -7,6 +7,33 @@ let demoTopK = 3;
 let demoMiniCategoryNumber = 10;
 
 
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        isIOS = navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+        dpr = isIOS ? Math.min(win.devicePixelRatio, 3) : 1,
+        dpr = window.top === window.self ? dpr : 1, //被iframe引用时，禁止缩放
+        dpr = 1,
+        scale = 1 / dpr,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+    docEl.dataset.dpr = dpr;
+    var metaEl = doc.createElement('meta');
+    metaEl.name = 'viewport';
+    metaEl.content = 'initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale;
+    docEl.firstElementChild.appendChild(metaEl);
+    var recalc = function() {
+        var width = docEl.clientWidth;
+        if (width / dpr > 750) {
+            width = 750 * dpr;
+        }
+        // 乘以100，px : rem = 100 : 1
+        docEl.style.fontSize = 100 * (width / 750) + 'px';
+    };
+    recalc()
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+})(document, window);
+
+
 /**
  * Initial the drawing canvas
  */
@@ -141,7 +168,7 @@ function performPrediction() {
     for (let i = 0; i < topK; i++) {
       let index = indices[i];
       predictionText += categoryNames[index];
-      predictionText += '<span style="font-size:14px;">(匹配度';
+      predictionText += '<span style="font-size:0.14rem;">(匹配度';
       predictionText += (probabilities[index]*100).toFixed(2);
       predictionText += '%)</span>';
       if (i < demoTopK - 1)
